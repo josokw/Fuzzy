e654aaac84414d9d1138149a620ca47da7f64125
josokw <jos.onokiewicz@han.nl>
2016-02-13 20:29:53 +0100

    Added to DySySim simulation blocks a virtual dtor.

--- ControllerFOProcess/main.cpp
+++ ControllerFOProcess/main.cpp
@@ -1,6 +1,7 @@
 #include "AppInfo.h"
 #include "RCcircuit.h"
 #include "FuzzyController.h"
+#include "LibInfoDySySim.h"
 #include <iomanip>
 #include <iostream>
 
@@ -10,33 +11,34 @@ namespace dss = dysysim;
 
 int main()
 {
-    cout << "-- " APPNAME_VERSION " " << string(50, '-') << endl << endl;
-    const double Tsample{0.1};
-    const double Tsimulation{Tsample / 10};
+    cout << "-- " APPNAME_VERSION " " << string(50, '-') << endl
+         << "-- uses " + dss::libName + " " << dss::libVersion << endl << endl;
+    const double Tsample{0.005};
+    const double Tsimulation{Tsample};
     const double stepTime = 10 * Tsimulation;
     const double RCtime{0.2};
     // Model
     dss::Time time{Tsimulation};
-    dss::Step STEP{0, 2000.0, stepTime};
+    dss::Step STEP{0, 2048, 0 /*stepTime*/};
     FuzzyController fuzzyC;
     dss::ZeroOrderHold zoh{5};
     RCcircuit RC{0, RCtime};
 
     double setpoint{0.0};
     double error{0.0};
-    double output{0.0};
+    double control{0.0};
 
-    for(int tn = 0; tn < 400; ++tn) {
+    for(int tn = 0; tn < 600; ++tn) {
         setpoint = STEP.output();
         error = setpoint - RC.output();
-        output = fuzzyC.inferControl(error);
-        zoh.input(error);
-        cout << "ZOH = " << zoh.output() << endl;
+        control = fuzzyC.inferControl(error);
+        zoh.input(control);
+        cout << "ZOH = " << int(zoh.output()) << endl;
         RC.input(zoh.output());
 
         cout << "t = " << setw(5) << time.output()
-             << "  input = " << setw(6) << setpoint
-             << "  output = " << setw(10) << fixed << setprecision(2)
+             << "  Setpoint = " << setw(6) << setpoint
+             << "  Measured Value = " << setw(10)
              << RC.output() << endl;
 
         time.next();
--- DySySim/DySySim.h
+++ DySySim/DySySim.h
@@ -11,6 +11,7 @@ namespace dysysim {
 class Time: public TimedSimBlock {
 public:
    Time(double TsimStep): TimedSimBlock{} { CommonTime::TsimStep = TsimStep; }
+   virtual ~Time() = default;
    void next() { Time::tc.next(); }
    double output() const override { return Time::tc.t; }
 };
@@ -20,6 +21,7 @@ public:
    Step(double initialValue, double stepValue, double stepTime):
       TimedSimBlock{},
       _initialValue{initialValue}, _stepValue{stepValue},_stepTime{stepTime} {}
+   virtual ~Step() = default;
    double output() const override { return tc.t >= _stepTime
                                     ? _stepValue : _initialValue; }
 private:
@@ -32,6 +34,7 @@ class ZeroOrderHold: public TimedSimBlock {
 public:
    ZeroOrderHold(int nSamples = 1):
       TimedSimBlock{}, _nSamples{nSamples}, _sample{0} {}
+   virtual ~ZeroOrderHold() = default;
    void input(double in) { _out = (_sample++ % _nSamples == 0) ? in : _out; }
    double output() const override { return _out; }
 private:
@@ -45,7 +48,7 @@ class Integrator: public TimedSimBlock {
 public:
    Integrator(double initValue):
       TimedSimBlock{}, _initValue{initValue}, _out{initValue} {}
-   ~Integrator() = default;
+   virtual ~Integrator() = default;
    void input(double in) { _out += in * tc.TsimStep; }
    double output() const { return _out; }
    void reset() { _out = _initValue; }
