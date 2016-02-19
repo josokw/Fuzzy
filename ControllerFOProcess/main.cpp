0bb32cab70813185e91b89723d99cfc23a679f43
josokw <jos.onokiewicz@han.nl>
2016-02-19 06:13:02 +0100

    Step triggering by calling input().

--- ControllerFOProcess/main.cpp
+++ ControllerFOProcess/main.cpp
@@ -11,47 +11,48 @@ namespace dss = dysysim;
 
 int main()
 {
-    cout << "-- " APPNAME_VERSION " " << string(50, '-') << endl
-         << "-- uses " + dss::libName + " " << dss::libVersion << endl << endl;
-    const double Tsample{0.005};
-    const double Tsimulation{Tsample};
-//  const double stepTime = 10 * Tsimulation;
-    const double RCtime{0.47};
-    // Model
-    dss::Time time{1, Tsimulation};
-    dss::Step STEP{2, 0, 2048, 0 /*stepTime*/};
-    dss::Summator sum{3};
+   cout << "-- " APPNAME_VERSION " " << string(50, '-') << endl
+        << "-- uses " + dss::libName + " " << dss::libVersion << endl << endl;
 
-    FuzzyController fuzzyController;
-    dss::Function<double> fuzzyC(4, [&fuzzyController](double in){ return fuzzyController.inferControl(in); });
-    //dss::ZeroOrderHold zoh{5, 1};
-    dss::FirstOrder RCcircuit{10, RCtime};
+   const double Tsample{0.005};  // 5 msec
+   const double Tsimulation{Tsample};
+   //  const double stepTime = 10 * Tsimulation;
+   const double RCtime{0.47}; // tau 0.47 sec
+   // Model
+   dss::Time time{1, Tsimulation};
+   dss::Step step{2, 0, 2048, 0 /*stepTime*/};
+   dss::Summator sum{3};
+   FuzzyController fuzzyController;
+   dss::Function<double> fuzzyC(4, [&fuzzyController](double in){ return fuzzyController.inferControl(in); });
+   //dss::ZeroOrderHold zoh{5, 1};
+   dss::FirstOrder RCcircuit{10, RCtime};
 
-    double setpoint{0.0};
-    double error{0.0};
-    double control{0.0};
+   double setpoint{0.0};
+   double error{0.0};
+   double control{0.0};
 
-    for(int tn = 0; tn < 600; ++tn) {
-        setpoint = STEP.output();
-        sum.input(setpoint, -RCcircuit.output());
-        error = sum.output();
-        //control = fuzzyC.inferControl(error);
-        fuzzyC.input(error);
-        control = fuzzyC.output();
-        //zoh.input(control);
-        //cout << "ZOH = " << int(zoh.output()) << endl;
-        //RCcircuit.input(zoh.output());
-        RCcircuit.input(control);
+   for(int tn = 0; tn < 600; ++tn) {
+      step.input(0);
+      setpoint = step.output();
+      sum.input(setpoint, -RCcircuit.output());
+      error = sum.output();
+      //control = fuzzyC.inferControl(error);
+      fuzzyC.input(error);
+      control = fuzzyC.output();
+      //zoh.input(control);
+      //cout << "ZOH = " << int(zoh.output()) << endl;
+      //RCcircuit.input(zoh.output());
+      RCcircuit.input(control);
 
-        cout << "t = " << setw(5) << time.output()
-             << "  Setpoint = " << setw(6) << setpoint
-             << "  Control = " << setw(6) << control
-             << "  Measured Value = " << setw(10)
-             << RCcircuit.output() << endl;
+      cout << "t = " << setw(5) << time.output()
+           << "  Setpoint = " << setw(6) << setpoint
+           << "  Control = " << setw(6) << control
+           << "  Measured Value = " << setw(10)
+           << RCcircuit.output() << endl;
 
-        time.next();
-        getchar();
-    }
+      time.next();
+      getchar();
+   }
 
-    return 0;
+   return 0;
 }
