010403d62210286b99dbcdafaacebd8c59eb646f
Jos Onokiewicz <jos.onokiewicz@gmail.com>
2021-07-15 16:15:16 +0200

    Initial version DySySim: dss

---
+++ appDySySim/main.cpp
@@ -0,0 +1,60 @@
+#include "AppInfo.h"
+#include "LibInfoDySySim.h"
+
+#include <boost/spirit/home/x3.hpp>
+
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+namespace x3 = boost::spirit::x3;
+namespace dss = dysysim;
+
+int main(int argc, char *argv[])
+{
+   std::cout << "-- " APPNAME_VERSION " " << std::string(50, '-') << std::endl
+             << "-- uses " + dss::libName + " " << dss::libVersion << std::endl
+             << std::endl;
+
+   if (argc > 2) {
+      return 1;
+   }
+
+   std::ifstream programFile("../appDySySim/data/RCnetwork.dss");
+
+   std::vector<std::string> program;
+
+   if (programFile.is_open())
+   {
+      std::string line;
+      while(getline(programFile, line))
+      {
+         program.push_back(line);
+         std::cout << line << "\n";
+      }
+   }
+
+   std::cout << "\n-- Syntax check\n";
+
+   for (auto input : program) {
+      auto iter = begin(input);
+      auto iterEnd = end(input);
+
+      auto value = x3::double_;
+      auto c_name = x3::alpha >> *(x3::alpha | x3::char_('_') | x3::digit);
+      auto set_const = c_name >> '=' >> value;
+      auto input_indices = x3::int_ >> *(x3::char_(',') >> x3::int_);
+
+      auto dssLine =
+         x3::uint_ >> +x3::char_("A-Z") >> *input_indices >> *set_const;
+
+      auto p = x3::phrase_parse(iter, iterEnd, dssLine, x3::space);
+
+      std::cout << input
+                << ((p and iter == iterEnd) ? "   OK  " : "   NOT OK  ")
+                << "\n";
+   }
+
+   return 0;
+}
