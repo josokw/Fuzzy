7065d30a7ed46c7a8da093c317c8dba81010e799
Jos Onokiewicz <jos.onokiewicz@gmail.com>
2021-09-14 13:12:05 +0200

    Update code to latest libDySySim

--- appControllerFOProcess/main.cpp
+++ appControllerFOProcess/main.cpp
@@ -2,57 +2,67 @@
 #include "DySySim.h"
 #include "FuzzyController.h"
 #include "LibInfoDySySim.h"
+#include "SimBlockFactory.h"
 
+#include <exception>
 #include <iomanip>
 #include <iostream>
 
 namespace dss = dysysim;
 
-/// Under construction ....
+inline double getOutput(int id)
+{
+   return dss::SimBlock::getSimBlock(id)->output();
+}
+
+/// \todo  Under construction ....
 int main(int argc, char *argv[])
 {
    std::cout << "-- " APPNAME_VERSION " "
              << "-- uses " + dss::libName + " " << dss::libVersion << "\n\n";
 
-   const double RCtime{0.47}; // FOP tau 0.47 sec
-   dss::SimTime::set(0.005, 20.0);
+   try {
+      dss::SimBlockFactory sbf;
+      sbf.init();
 
-   // Model
-   dss::Step step;
-   step.config({1, {}, {0, 2055, 0.01}});
-   dss::Summator sum;
-   sum.config({2, {1, -4}, {}});
+      FuzzyController fuzzyController;
+      auto pFuzzyC = std::make_shared<dss::Function<double>>();
+      pFuzzyC->setFunction([&fuzzyController](double in) {
+         return fuzzyController.inferControl(in);
+      });
+      sbf.add("FUZZYC", pFuzzyC);
 
-   FuzzyController fuzzyController;
-   dss::Function<double> fuzzyC;
-   fuzzyC.setFunction([&fuzzyController](double in) {
-      return fuzzyController.inferControl(in);
-   });
-   fuzzyC.config({3, {2}, {}});
+      const double RCtime{0.47};
+      dss::SimTime::set(0.005, 20.0);
+      sbf.configCheck("STP", {1, {}, {0, 2055, 0.1}});
+      sbf.configCheck("SUM", {2, {1, -4}, {}});
+      sbf.configCheck("FUZZYC", {3, {2}, {}});
+      sbf.configCheck("FIO", {4, {3}, {RCtime, 0.0}});
 
-   dss::FirstOrder RCcircuit;
-   RCcircuit.config({4, {3}, {RCtime, 0}});
-
-   std::ofstream simdata;
-   if (argc == 2) {
-      simdata.open(argv[1]);
-   }
-
-   dss::SimBlock::setExeSequence();
-   dss::SimBlock::initSimBlocks();
-   do {
-      std::cout << "t = " << std::setw(5) << dss::SimBlock::sim_time.t
-                << "  Setpoint = " << std::setw(6) << step.output()
-                << "  Control = " << std::setw(6) << fuzzyC.output()
-                << "  Measured Value = " << std::setw(10) << RCcircuit.output()
-                << std::endl;
+      std::ofstream simdata;
       if (argc == 2) {
-         simdata << dss::SimBlock::sim_time.t << " " << step.output() << " "
-                 << fuzzyC.output() << " " << RCcircuit.output() << std::endl;
+         simdata.open(argv[1]);
       }
-   } while (dss::SimTime::simulation_on());
 
-   simdata.close();
+      dss::SimBlock::setExeSequence();
+      dss::SimBlock::initSimBlocks();
+      do {
+         std::cout << "t = " << std::setw(5) << dss::SimBlock::sim_time.t
+                   << "  Setpoint = " << std::setw(6) << getOutput(1)
+                   << "  Control = " << std::setw(6) << getOutput(3)
+                   << "  Measured Value = " << std::setw(10) << getOutput(4)
+                   << std::endl;
+         if (argc == 2) {
+            simdata << dss::SimBlock::sim_time.t << " " << getOutput(1) << " "
+                    << getOutput(3) << " " << getOutput(4) << std::endl;
+         }
+      } while (dss::SimTime::simulation_on());
+
+      simdata.close();
+   }
+   catch (std::exception &e) {
+      std::cerr << "Exception: " << e.what() << "\n";
+   }
 
    return 0;
 }
--- appRC-OnOff/main.cpp
+++ appRC-OnOff/main.cpp
@@ -4,6 +4,7 @@
 #include "LibInfoDySySim.h"
 #include "SimBlockFactory.h"
 
+#include <exception>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
@@ -20,48 +21,43 @@ int main(int argc, char *argv[])
    std::cout << "-- " APPNAME_VERSION " "
              << "-- uses " + dss::libName + " " << dss::libVersion << "\n\n";
 
-   dss::SimBlockFactory sbf;
-   sbf.init();
-   // Initial conditions
-   const double delta_t{0.005};
-   const double RCtime{0.5};
-   dss::SimTime::set(delta_t, 5 * RCtime);
-   sbf.configCheck("STP", {1, {}, {0, 3000, 0.1}});
-   sbf.configCheck("SUM", {2, {1, -4}, {}});
-   sbf.configCheck("ONOFF", {3, {2}, {0, 4000, 0}});
-   sbf.configCheck("FIO", {4, {3}, {RCtime, 2500.0}});
+   try {
+      dss::SimBlockFactory sbf;
+      sbf.init();
+      const double delta_t{0.005};
+      const double RCtime{0.5};
+      dss::SimTime::set(delta_t, 5 * RCtime);
+      sbf.configCheck("STP", {1, {}, {0, 3000, 0.1}});
+      sbf.configCheck("SUM", {2, {1, -4}, {}});
+      sbf.configCheck("ONOFF", {3, {2}, {0, 4000, 0}});
+      sbf.configCheck("FIO", {4, {3}, {RCtime, 2500.0}});
 
-   // dss::Step step;
-   // step.config({1, {}, {0, 3000, 0.1}});
-   // dss::Summator sum;
-   // sum.config({2, {1, -4}, {}});
-   // dss::OnOff onoff;
-   // onoff.config({3, {2}, {0, 4000, 0}});
-   // dss::FirstOrder RCcircuit;
-   // RCcircuit.config({4, {3}, {RCtime, 2500.0}});
-
-   std::ofstream simdata;
-   if (argc == 2) {
-      simdata.open(argv[1]);
-   }
-
-   dss::SimBlock::setExeSequence();
-   dss::SimBlock::initSimBlocks();
-   do {
-      std::cout << "  t = " << std::setw(5) << dss::SimBlock::sim_time.t
-                << "  Setpoint = " << std::setw(5) << getOutput(1)
-                << "  Control = " << std::setw(5) << getOutput(3)
-                << "  Measured Value = " << std::setw(8) << getOutput(4)
-                << std::endl;
-
+      std::ofstream simdata;
       if (argc == 2) {
-         simdata << std::setw(4) << dss::SimBlock::sim_time.t << " "
-                 << getOutput(1) << " " << getOutput(2) << " "
-                 << getOutput(4) << std::endl;
+         simdata.open(argv[1]);
       }
-   } while (dss::SimTime::simulation_on());
 
-   simdata.close();
+      dss::SimBlock::setExeSequence();
+      dss::SimBlock::initSimBlocks();
+      do {
+         std::cout << "  t = " << std::setw(5) << dss::SimBlock::sim_time.t
+                   << "  Setpoint = " << std::setw(5) << getOutput(1)
+                   << "  Control = " << std::setw(5) << getOutput(3)
+                   << "  Measured Value = " << std::setw(8) << getOutput(4)
+                   << std::endl;
+
+         if (argc == 2) {
+            simdata << std::setw(4) << dss::SimBlock::sim_time.t << " "
+                    << getOutput(1) << " " << getOutput(2) << " "
+                    << getOutput(4) << std::endl;
+         }
+      } while (dss::SimTime::simulation_on());
+
+      simdata.close();
+   }
+   catch (std::exception &e) {
+      std::cerr << "Exception: " << e.what() << "\n";
+   }
 
    return 0;
 }
