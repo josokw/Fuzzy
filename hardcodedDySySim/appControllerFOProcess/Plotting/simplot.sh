9fc4c5e95414aef3acee7a43af865be0df79cd3b
Jos Iduna <jos.onokiewicz@gmail.com>
2023-07-27 09:27:55 +0200

    Move DySySim examples to hardcodedDySySim

--- hardcodedDySySim/appControllerFOProcess/Plotting/simplot.sh
+++ hardcodedDySySim/appControllerFOProcess/Plotting/simplot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sim_name="RC-OnOff"
+sim_name="ControllerFOProcess"
 
 simresult="sim"$sim_name".txt"
 
@@ -8,9 +8,8 @@ echo
 echo ".. Start" $sim_name "simulation"
 echo "..   data to $simresult"
 
-../../bin/$sim_name $simresult
+../../../bin/$sim_name $simresult
 gnuplot -persist < plot.plt
 
 echo ".. Ready" $sim_name "simulation"
 echo
-
--- hardcodedDySySim/appRC-OnOff/Plotting/simplot.sh
+++ hardcodedDySySim/appRC-OnOff/Plotting/simplot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sim_name="ControllerFOProcess"
+sim_name="RC-OnOff"
 
 simresult="sim"$sim_name".txt"
 
@@ -8,8 +8,9 @@ echo
 echo ".. Start" $sim_name "simulation"
 echo "..   data to $simresult"
 
-../../bin/$sim_name $simresult
+../../../bin/$sim_name $simresult
 gnuplot -persist < plot.plt
 
 echo ".. Ready" $sim_name "simulation"
 echo
+
